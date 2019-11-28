3b06ca86d7f3bb8f35e4e34035529a6f0f111e7c
Anton Georgiev <antongeorgiev313@gmail.com>
2019-11-28 20:31:57 +0200

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,11 +1,21 @@
 import os
-
+print()
+print()
+print()
 
 # - Create the ./media folder:
-
+print("Creating the media folder...")
+if not os.path.exists('media'):
+    os.mkdir('./media');
+    print("- Done!")
+else:
+    print("- ./media already exists, passing this step")
 
 # - Create the settings file:
-bucket_name = input("Enter your S3 bucket name: ")
+print()
+print()
+print("Setting up bucket name...")
+bucket_name = input("- Please enter the S3 bucket name: ")
 saveFile = open('./settings.txt', 'w')
 saveFile.write('Bucket name:')
 saveFile.write('\n')
