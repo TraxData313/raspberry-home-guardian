9ee102a4f3aac1b77cb24b768220ed398c2bbba5
Anton Georgiev <antongeorgiev313@gmail.com>
2019-12-02 11:55:59 +0200

    Update rpi_home_guardian.py

--- rpi_home_guardian.py
+++ rpi_home_guardian.py
@@ -1,6 +1,10 @@
 import functions
 import time
+from loggers import log_error, log_event
 
+reporting_program_name = 'rpi_home_guardian.py'
+event_message = 'Starting program'
+log_event(reporting_program_name, event_message)
 
 while True:
     print()
