db151d3c7c79f49a515c5bcc9bb2ab3dc1672f6b
Anton Georgiev <antongeorgiev313@gmail.com>
2019-12-02 16:42:46 +0200

    sh files

---
+++ installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# How to use:
+# Run this file by entering commands:
+# chmod +x ./installer.sh
+# sudo ./installer.sh
+
+
+# - Initial message:
+echo "Installing rpi_home_guardian service..."
+
+# - Start the configure.py script to set up S3 bucket:
+echo "Start the configure.py script to set up the S3 bucket and initialize the ./media folder..."
+python configure.py
+echo "- Done!"
+
+# - Place the rpi_home_guardian.service in /etc/systemd/system:
+echo "Place the rpi_home_guardian.service in /etc/systemd/system..."
+mv ./rpi_home_guardian.service /etc/systemd/system/rpi_home_guardian.service
+echo "- Done!"
+
+# - Make the service start at boot and start it now:
+echo "Make the service start at boot, reload daemons and starting it now..."
+systemctl enable rpi_home_guardian.service
+systemctl daemon-reload
+systemctl start rpi_home_guardian.service
+echo "- Done!"
+
+# - Final message:
+echo "-- All done!"
---
+++ rpi_home_guardian.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ~/raspberry-home-guardian-with-aws/rpi_home_guardian.py
