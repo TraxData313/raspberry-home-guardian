d64e66c522e630ec955cc2b5a27a993e0f42ef05
Anton Georgiev <antongeorgiev313@gmail.com>
2019-12-02 11:53:36 +0200

    Update rpi_home_guardian.py

--- rpi_home_guardian.py
+++ rpi_home_guardian.py
@@ -3,12 +3,15 @@ import time
 
 
 while True:
+    print()
     # - Check for files ./media and upload them:
     functions.check_upload_files()
 
     # - Detect motion:
     motion_bool = functions.motion_detect()
     if motion_bool == True:
+        print("- Motion detected!")
         functions.take_video(5)
     else:
+        print("- No motion, looking...")
         time.sleep(1)
